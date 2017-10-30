32945c4bfaa912941d5387fd91504ef36d16a72e
Jeremy Schlatter <jeremy.schlatter@gmail.com>
2017-10-30 13:26:18 -0700

    buggy implementation of policy iteration

--- demonstrator.py
+++ demonstrator.py
@@ -1,7 +1,7 @@
 import torch
 
 
-def policy(reward_function, transition_beliefs):
+def policy(reward_function, transition_beliefs, discount):
     '''
     reward_function: state, action -> real
     transition_beliefs: state, action -> Δstate
@@ -10,7 +10,47 @@ def policy(reward_function, transition_beliefs):
     n_states, n_actions = reward_function.shape
     assert (n_states, n_actions, n_states) == transition_beliefs.shape
 
-    return torch.Tensor(n_states, n_actions)
+    v = torch.Tensor(n_states)
+    pi = torch.Tensor(n_states, n_actions)
+
+    # policy iteration
+    while True:
+        # policy evaluation (update v)
+        while True:
+            delta = 0
+            # TODO: vectorize
+            for i in range(n_states):
+                tmp = v[i]
+
+                next_state_dist = transition_beliefs[i].t() @ pi[i]
+                next_state_value = next_state_dist @ (discount * v)
+                this_state_value = reward_function[i] @ pi[i]
+
+                v[i] = this_state_value + next_state_value
+                delta = max(delta, abs(tmp - v[i]))
+
+            if delta < 0.001:
+                break
+
+        # policy improvement (update pi)
+        stable = True
+        # TODO: vectorize
+        for i in range(n_states):
+            tmp = pi[i]
+            # TODO: vectorize instead of comprehension
+            a_values = [
+                reward_function[i, a] + discount * (transition_beliefs[i, a] @ v)
+                for a in range(n_actions)
+            ]
+            var = torch.autograd.Variable(torch.Tensor(a_values))
+            pi[i] = torch.nn.functional.softmax(var).data
+            if torch.max(tmp, 0)[1][0] != torch.max(pi[i], 0)[1][0]:
+                stable = False
+
+        if stable:
+            break
+
+    return pi
 
 
 if __name__ == '__main__':
@@ -20,6 +60,13 @@ if __name__ == '__main__':
         [[1, 0, 0], [0, 0, 1]],
         [[0, 1, 0], [0, 0, 1]],
     ])
+    discount = 0.9
 
-    pi = policy(reward_function, transition_beliefs)
-    print(pi == torch.Tensor([[0, 1], [0, 1], [0, 1]]))
+    pi = policy(reward_function, transition_beliefs, discount)
+    want = torch.Tensor([[0, 1], [0, 1], [0, 1]])
+    print(pi == want)
+    print()
+    print('got:')
+    print(pi)
+    print('want:')
+    print(want)
