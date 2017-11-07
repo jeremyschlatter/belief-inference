6b27253be58c1ef8a8d9d1ff47db15ee39c25bd4
Jeremy Schlatter <jeremy.schlatter@gmail.com>
2017-11-06 16:50:21 -0800

    implement demonstrate

--- mdp_solver.py
+++ mdp_solver.py
@@ -1,5 +1,14 @@
 import torch
 
+from torch.autograd import Variable
+
+
+def _to_variable(x):
+    if isinstance(x, torch.Tensor):
+        return Variable(x)
+    assert isinstance(x, Variable)
+    return x
+
 
 def policy(reward_function, transition_beliefs, discount, max_iters=100):
     '''
@@ -8,16 +17,14 @@ def policy(reward_function, transition_beliefs, discount, max_iters=100):
     policy result: state -> Δaction
     '''
     # reward_function and transition_beliefs could be either Tensors or Variables.
-    if isinstance(reward_function, torch.Tensor):
-        reward_function = torch.autograd.Variable(reward_function)
-    if isinstance(transition_beliefs, torch.Tensor):
-        transition_beliefs = torch.autograd.Variable(transition_beliefs)
+    reward_function = _to_variable(reward_function)
+    transition_beliefs = _to_variable(transition_beliefs)
 
-    n_states, n_actions = reward_function.data.shape
-    assert (n_states, n_actions, n_states) == transition_beliefs.data.shape
+    n_states, n_actions = reward_function.size()
+    assert (n_states, n_actions, n_states) == transition_beliefs.size()
 
-    v = torch.autograd.Variable(torch.zeros(n_states), requires_grad=False)
-    pi = torch.autograd.Variable(torch.ones(n_states, n_actions) / n_actions, requires_grad=False)
+    v = Variable(torch.zeros(n_states), requires_grad=False)
+    pi = Variable(torch.ones(n_states, n_actions) / n_actions, requires_grad=False)
 
     # policy iteration
     for i in range(max_iters):
@@ -39,9 +46,37 @@ def policy(reward_function, transition_beliefs, discount, max_iters=100):
     return pi
 
 
+def demonstrate(t_real, t_belief, r, discount, n, length=50):
+    t_real = _to_variable(t_real)
+    t_belief = _to_variable(t_belief)
+    r = _to_variable(r)
+
+    n_states, n_actions = r.size()
+    assert (n_states, n_actions, n_states) == t_real.size()
+    assert (n_states, n_actions, n_states) == t_belief.size()
+
+    pi = policy(r, t_belief, discount)  # .expand(n, n_states, n_actions)
+    states = Variable(torch.zeros(n).long())
+    trajs = None
+
+    for _ in range(length):
+        action_dists = pi.index_select(0, states)
+        actions = action_dists.multinomial(1).long()
+        frame = torch.cat((states[:, None], actions), 1)[:, None, :]
+
+        trajs = frame if trajs is None else torch.cat((trajs, frame), 1)
+
+        state_dists = t_real.index_select(0, states).gather(1, actions.view(n, 1, 1).expand(n, 1, n_states)).squeeze(1)
+        # TODO: better implementation supported by the next > 0.2.0_1 pytorch release
+        # state_dists = t_real[states, actions.squeeze(1)]
+        states = state_dists.multinomial(1).squeeze(1)
+
+    return trajs
+
+
 if __name__ == '__main__':
-    reward_function = torch.autograd.Variable(torch.Tensor([[-1, 1], [0, 1], [0, 2]]), requires_grad=False)
-    transition_beliefs = torch.autograd.Variable(torch.Tensor([
+    reward_function = Variable(torch.Tensor([[-1, 1], [0, 1], [0, 2]]), requires_grad=False)
+    transition_beliefs = Variable(torch.Tensor([
         [[1, 0, 0], [0, 1, 0]],
         [[1, 0, 0], [0, 0, 1]],
         [[0, 1, 0], [0, 0, 1]],
