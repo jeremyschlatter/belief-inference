c152b252ae6d81de8579b391e482339db37b77f7
Jeremy Schlatter <jeremy.schlatter@gmail.com>
2017-11-10 17:22:10 -0800

    try t_real = t_belief = plain ground

--- test_solver.py
+++ test_solver.py
@@ -58,26 +58,71 @@ def test_policy_gradient():
     assert loss.data[0] < 0.00001
 
 
-@manual_seed
-def test_belief_inference():
-    s, a = 20, 3
-    t_real = mdp.gpu(Variable(random_transitions(s, a)))
-    t_belief = mdp.gpu(Variable(random_transitions(s, a)))
-    R = mdp.gpu(torch.rand(s, a) * 10)  # noqa
+def _inference_test_helper(t_real, t_belief, r):
+    t_real = mdp._to_variable(t_real)
+    t_belief = mdp._to_variable(t_belief)
+    r = mdp._to_variable(r)
+
+    s, a = r.size()
     discount = 0.9
-    trajs = mdp.demonstrate(t_real, t_belief, R, discount, 1000)
+    trajs = mdp.demonstrate(t_real, t_belief, r, discount, 1000)
 
     def loss(guess):
         return ((t_belief - t_guess) ** 2).sum().data[0]
     t_guess = mdp.gpu(Variable(random_transitions(s, a), requires_grad=True))
     initial_loss = loss(t_guess)
 
-    t_guess = mdp.infer_belief(t_real, R, discount, trajs, initial_guess=t_guess)
+    t_guess = mdp.infer_belief(t_real, r, discount, trajs, initial_guess=t_guess)
     final_loss = loss(t_guess)
 
     print(f'initial loss: {initial_loss}\nfinal loss: {final_loss}')
     assert final_loss < initial_loss
 
 
+@manual_seed
+def test_belief_inference():
+    s, a = 64, 4
+    t_real = mdp.gpu(Variable(random_transitions(s, a)))
+    t_belief = mdp.gpu(Variable(random_transitions(s, a)))
+    r = mdp.gpu(torch.rand(s, a) * 10)
+    _inference_test_helper(t_real, t_belief, r)
+
+
+@manual_seed
+def test_plain_ground():
+    r, t = plain_ground()
+    _inference_test_helper(t, t, r)
+
+
+def plain_ground():
+    w = h = 8
+    s = w * h
+    actions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    a = len(actions)
+
+    def to_state(x, y):
+        return y * w + x
+
+    def from_state(state):
+        return state % w, state // w
+
+    def clip(x, low, high):
+        return min(max(x, low), high)
+
+    r = torch.zeros(s, a)
+    r[to_state(7, 6), 2] = 10
+    r[to_state(6, 7), 1] = 10
+
+    t = torch.zeros(s, a, s)
+    for state in range(s):
+        for a_i, (dx, dy) in enumerate(actions):
+            x, y = from_state(state)
+            x = clip(x + dx, 0, w - 1)
+            y = clip(y + dy, 0, h - 1)
+            t[state, a_i, to_state(x, y)] = 1
+
+    return r, t
+
+
 if __name__ == '__main__':
-    test_belief_inference()
+    test_plain_ground()
